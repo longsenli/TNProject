6128c2bcaf91f87c6479e31a9c0af076e29bb0ae
lightpeng123 <lightpeng123@163.com>
2018-12-27 10:17:15 +0800

    tb_user表新增字段，更新java类跟映射

--- src/main/java/com/tnpy/mes/mapper/mysql/TbUserMapper.java
+++ src/main/java/com/tnpy/mes/mapper/mysql/TbUserMapper.java
@@ -1,21 +1,29 @@
+
 package com.tnpy.mes.mapper.mysql;
 
-import com.tnpy.mes.model.mysql.TbUser;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
+
+import com.tnpy.mes.model.mysql.TbUser;
 
 @Mapper
 @Component
 public interface TbUserMapper {
-    int deleteByPrimaryKey(String userid);
+	int deleteByPrimaryKey(String userid);
 
-    int insert(TbUser record);
+	int insert(TbUser record);
 
-    int insertSelective(TbUser record);
+	int insertSelective(TbUser record);
 
-    TbUser selectByPrimaryKey(String userid);
+	TbUser selectByPrimaryKey(String userid);
 
-    int updateByPrimaryKeySelective(TbUser record);
+	@Select("select * from tb_user")
+	List<TbUser> userList();
 
-    int updateByPrimaryKey(TbUser record);
+	int updateByPrimaryKeySelective(TbUser record);
+
+	int updateByPrimaryKey(TbUser record);
 }
--- src/main/java/com/tnpy/mes/model/mysql/TbUser.java
+++ src/main/java/com/tnpy/mes/model/mysql/TbUser.java
@@ -3,83 +3,284 @@ package com.tnpy.mes.model.mysql;
 import java.util.Date;
 
 public class TbUser {
-    private String userid;
 
-    private String name;
+	private String userid;
 
-    private String password;
+	private String loginname;
 
-    private String roleid;
+	public String getLoginname() {
+		return loginname;
+	}
 
-    private String sex;
+	public void setLoginname(String loginname) {
+		this.loginname = loginname;
+	}
 
-    private Date birthday;
+	private String name;
 
-    private String dutyid;
+	private String password;
 
-    private String workplace;
+	private String roleid;
 
-    public String getUserid() {
-        return userid;
-    }
+	private String sex;
 
-    public void setUserid(String userid) {
-        this.userid = userid == null ? null : userid.trim();
-    }
+	private Date birthday;
 
-    public String getName() {
-        return name;
-    }
+	private String dutyid;
 
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
+	private String workplace;
 
-    public String getPassword() {
-        return password;
-    }
+	private Date createdate;
 
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
+	private String createuser;
 
-    public String getRoleid() {
-        return roleid;
-    }
+	private Date updatedate;
 
-    public void setRoleid(String roleid) {
-        this.roleid = roleid == null ? null : roleid.trim();
-    }
+	private String updateuser;
 
-    public String getSex() {
-        return sex;
-    }
+	private Date deletedate;
 
-    public void setSex(String sex) {
-        this.sex = sex == null ? null : sex.trim();
-    }
+	private String deleteuser;
 
-    public Date getBirthday() {
-        return birthday;
-    }
+	private Integer state;
 
-    public void setBirthday(Date birthday) {
-        this.birthday = birthday;
-    }
+	private String email;
 
-    public String getDutyid() {
-        return dutyid;
-    }
+	private String mobilephone;
 
-    public void setDutyid(String dutyid) {
-        this.dutyid = dutyid == null ? null : dutyid.trim();
-    }
+	private String telephone;
 
-    public String getWorkplace() {
-        return workplace;
-    }
+	private String organid;
 
-    public void setWorkplace(String workplace) {
-        this.workplace = workplace == null ? null : workplace.trim();
-    }
+	private String ext1;
+
+	private String ext2;
+
+	private String ext3;
+
+	private String ext4;
+
+	private String ext5;
+
+	private String ext6;
+
+	private String ext7;
+
+	private String ext8;
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid == null ? null : userid.trim();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name == null ? null : name.trim();
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password == null ? null : password.trim();
+	}
+
+	public String getRoleid() {
+		return roleid;
+	}
+
+	public void setRoleid(String roleid) {
+		this.roleid = roleid == null ? null : roleid.trim();
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex == null ? null : sex.trim();
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getDutyid() {
+		return dutyid;
+	}
+
+	public void setDutyid(String dutyid) {
+		this.dutyid = dutyid == null ? null : dutyid.trim();
+	}
+
+	public String getWorkplace() {
+		return workplace;
+	}
+
+	public void setWorkplace(String workplace) {
+		this.workplace = workplace == null ? null : workplace.trim();
+	}
+
+	public Date getCreatedate() {
+		return createdate;
+	}
+
+	public void setCreatedate(Date createdate) {
+		this.createdate = createdate;
+	}
+
+	public String getCreateuser() {
+		return createuser;
+	}
+
+	public void setCreateuser(String createuser) {
+		this.createuser = createuser == null ? null : createuser.trim();
+	}
+
+	public Date getUpdatedate() {
+		return updatedate;
+	}
+
+	public void setUpdatedate(Date updatedate) {
+		this.updatedate = updatedate;
+	}
+
+	public String getUpdateuser() {
+		return updateuser;
+	}
+
+	public void setUpdateuser(String updateuser) {
+		this.updateuser = updateuser == null ? null : updateuser.trim();
+	}
+
+	public Date getDeletedate() {
+		return deletedate;
+	}
+
+	public void setDeletedate(Date deletedate) {
+		this.deletedate = deletedate;
+	}
+
+	public String getDeleteuser() {
+		return deleteuser;
+	}
+
+	public void setDeleteuser(String deleteuser) {
+		this.deleteuser = deleteuser == null ? null : deleteuser.trim();
+	}
+
+	public Integer getState() {
+		return state;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email == null ? null : email.trim();
+	}
+
+	public String getMobilephone() {
+		return mobilephone;
+	}
+
+	public void setMobilephone(String mobilephone) {
+		this.mobilephone = mobilephone == null ? null : mobilephone.trim();
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone == null ? null : telephone.trim();
+	}
+
+	public String getOrganid() {
+		return organid;
+	}
+
+	public void setOrganid(String organid) {
+		this.organid = organid == null ? null : organid.trim();
+	}
+
+	public String getExt1() {
+		return ext1;
+	}
+
+	public void setExt1(String ext1) {
+		this.ext1 = ext1 == null ? null : ext1.trim();
+	}
+
+	public String getExt2() {
+		return ext2;
+	}
+
+	public void setExt2(String ext2) {
+		this.ext2 = ext2 == null ? null : ext2.trim();
+	}
+
+	public String getExt3() {
+		return ext3;
+	}
+
+	public void setExt3(String ext3) {
+		this.ext3 = ext3 == null ? null : ext3.trim();
+	}
+
+	public String getExt4() {
+		return ext4;
+	}
+
+	public void setExt4(String ext4) {
+		this.ext4 = ext4 == null ? null : ext4.trim();
+	}
+
+	public String getExt5() {
+		return ext5;
+	}
+
+	public void setExt5(String ext5) {
+		this.ext5 = ext5 == null ? null : ext5.trim();
+	}
+
+	public String getExt6() {
+		return ext6;
+	}
+
+	public void setExt6(String ext6) {
+		this.ext6 = ext6 == null ? null : ext6.trim();
+	}
+
+	public String getExt7() {
+		return ext7;
+	}
+
+	public void setExt7(String ext7) {
+		this.ext7 = ext7 == null ? null : ext7.trim();
+	}
+
+	public String getExt8() {
+		return ext8;
+	}
+
+	public void setExt8(String ext8) {
+		this.ext8 = ext8 == null ? null : ext8.trim();
+	}
 }
