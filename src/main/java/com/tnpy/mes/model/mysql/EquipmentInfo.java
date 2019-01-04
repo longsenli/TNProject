84131f64b6e0b6df256bdf07c8f2da107b42346b
longsenli <llsbenign@163.com>
2019-01-04 17:24:45 +0800

    修改时间显示格式

--- src/main/java/com/tnpy/mes/model/mysql/EquipmentInfo.java
+++ src/main/java/com/tnpy/mes/model/mysql/EquipmentInfo.java
@@ -1,5 +1,7 @@
 package com.tnpy.mes.model.mysql;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
 import java.util.Date;
 
 public class EquipmentInfo {
@@ -10,7 +12,7 @@ public class EquipmentInfo {
     private String typeid;
 
     private String manufacturers;
-
+    @JSONField(format ="yyyy-MM-dd HH:mm:ss")
     private Date buytime;
 
     private String location;
