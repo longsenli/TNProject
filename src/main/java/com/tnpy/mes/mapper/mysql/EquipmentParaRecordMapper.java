65c3b0e529a5e0d4abdccc2d7070fc43a543a115
longsenli <llsbenign@163.com>
2019-02-22 10:17:24 +0800

    优化设备最新参数查询语句

--- src/main/java/com/tnpy/mes/mapper/mysql/EquipmentParaRecordMapper.java
+++ src/main/java/com/tnpy/mes/mapper/mysql/EquipmentParaRecordMapper.java
@@ -27,6 +27,14 @@ public interface EquipmentParaRecordMapper {
 @Select("select * from tb_equipmentparamrecord where equipmentID = #{equipID} order by recordTime desc,paramID asc limit 1000")
     List<EquipmentParaRecord> selectRecord( String equipID);
 
+/*
+*  <select id="selectLatestRecord" resultMap="ParamLatestRecordResultMap">
+    select a.equipmentID as equipmentID,b.name as equipName,b.location as equipLocation , a.recordTime as recordTime,a.value as value,a.status as status,a.recorder as recorder from ( (
+    select x.equipmentID,x.recordTime,x.status,x.value,x.recorder from tb_equipmentparamrecord x where x.paramID=#{paramID} and x.equipmentTypeID =#{equipType} and  recordTime =
+    (select max(recordTime) from tb_equipmentparamrecord y where  y.equipmentID=x.equipmentID)
+      ) as a left join tb_equipmentinfo b on a.equipmentID = b.id) where b.plantID = #{plantID} order by equipName desc
+  </select>
+  */
     List<EquipParamLatestRecord> selectLatestRecord(String plantID, String equipType, String paramID);
 
     @Select("select * from tb_equipmentparamrecord where equipmentID = #{equipID} and paramID = #{paramID} and recordTime >= #{startTime} " +
