c4c649eafe7964118803f87636e26777f5fc3585
longsenli <llsbenign@163.com>
2019-05-05 13:51:20 +0800

    屏蔽消息推送冲突

--- src/main/java/com/tnpy/mes/service/pushNotification/WebSocketConfig.java
+++ src/main/java/com/tnpy/mes/service/pushNotification/WebSocketConfig.java
@@ -6,15 +6,15 @@ package com.tnpy.mes.service.pushNotification;
  * @Date: 2019/1/19 14:12
  */
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebSocketConfig {
+    /*  正式使用时需要放开，与tomcat自身'serverEndpointExporter' 冲突
     @Bean
     public ServerEndpointExporter serverEndpointExporter()  {
+
         return new ServerEndpointExporter();
     }
-
+*/
 }
