d99d148c62d7862c0e85ea468b236d44aab91d72
longsenli <llsbenign@163.com>
2019-06-24 11:06:36 +0800

    socket修改

--- src/main/java/com/tnpy/mes/service/pushNotification/WebSocketConfig.java
+++ src/main/java/com/tnpy/mes/service/pushNotification/WebSocketConfig.java
@@ -6,10 +6,18 @@ package com.tnpy.mes.service.pushNotification;
  * @Date: 2019/1/19 14:12
  */
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-@Configuration
-public class WebSocketConfig {
+@Configurable
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+    }
     /*  正式使用时需要放开，与tomcat自身'serverEndpointExporter' 冲突
     @Bean
     public ServerEndpointExporter serverEndpointExporter()  {
--- src/main/java/com/tnpy/mes/service/pushNotification/impl/websocketManageService.java
+++ src/main/java/com/tnpy/mes/service/pushNotification/impl/websocketManageService.java
@@ -36,12 +36,13 @@ public class websocketManageService {
       //  System.out.println(JSONObject.toJSON(session).toString());
         webSocketSet.add(this);     //加入set中
         addOnlineCount();           //在线数加1
+        /*
         System.out.println("有新连接加入！当前在线人数为" + getOnlineCount());
         try {
             sendMessage("当前在线人数为" + getOnlineCount());
         } catch (IOException e) {
             System.out.println("IO异常");
-        }
+        }*/
     }
 
     /**
@@ -90,8 +91,10 @@ public class websocketManageService {
     public static void sendInfo(String message) throws IOException {
         for (websocketManageService item : webSocketSet) {
             try {
+
                 item.sendMessage(message);
             } catch (IOException e) {
+                System.out.println("============Wrong" + e.getMessage());
                 continue;
             }
         }
