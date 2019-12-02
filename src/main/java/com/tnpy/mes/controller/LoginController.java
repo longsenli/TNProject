1f9943addf3f51edc84f6114123b9e1f0617968e
longsenli <llsbenign@163.com>
2019-12-02 09:37:08 +0800

    晚上登录token和版本号

--- src/main/java/com/tnpy/mes/controller/LoginController.java
+++ src/main/java/com/tnpy/mes/controller/LoginController.java
@@ -62,6 +62,24 @@ public class LoginController {
 			return  response;
 		}
 	}
+
+	@RequestMapping(value = "/getAPPVersionWithParam")
+	public TNPYResponse getAPPVersionWithParam(String clientType,String userID,String plantID,String processID ) {
+		TNPYResponse response = new TNPYResponse();
+		try
+		{
+			String version = softwareVersionMapper.selectLatestVersion(clientType);
+			response.setStatus(StatusEnum.ResponseStatus.Success.getIndex());
+			response.setMessage(version);
+			return  response;
+		}
+		catch (Exception ex)
+		{
+			response.setMessage("0");
+			return  response;
+		}
+	}
+
 	@RequestMapping(value = "/getappversionbyclienttype")
 	public TNPYResponse getAppVersionByClientType( String clientType) {
 		TNPYResponse response = new TNPYResponse();
@@ -167,6 +185,96 @@ public class LoginController {
 			response.setMessage(myUser.getName() + " ### " + myUser.getRoleid() + " ### " + myUser.getIndustrialplant_id()
 					+ " ### " +  myUser.getProductionprocess_id() + " ### " + myUser.getProductionline_id() + "###" + myUser.getWorklocation_id() + "###-1" );
 
+		}
+		response.setStatus(StatusEnum.ResponseStatus.Success.getIndex());
+		response.setToken(JSONObject.toJSON(token).toString());
+
+		return response;
+	}
+
+	@RequestMapping(value = "/appLogin")
+	public TNPYResponse login( HttpServletRequest request,String username,String password,String appVersion) {
+//System.out.println(username + "===" +appVersion +"===="+request  );
+		TNPYResponse response = new TNPYResponse();
+		Encryption encryption = new Encryption();
+		//判断用户信息为空
+		if ("".equals(username.trim()) || "".equals(password.trim())) {
+			response.setMessage("传入的用户名/密码为空！");
+			return response;
+		}
+
+		//根据客户用户名查找数据库的usre对象
+		TbUser myUser = tbUserService.getUserInfo(username);
+
+		//判断用户不存在
+		if (null == myUser) {
+			response.setMessage("用户不存在");
+			return response;
+		}
+		//判断用户不存在
+		if (!password.trim().equals(myUser.getPassword()) && !encryption.encrypt(password.trim(),null).equals(myUser.getPassword())) {
+			response.setMessage("密码不正确");
+			return response;
+		}
+
+		//根据数据库的用户信息查询Token
+		Token token =  tokenmapper.findByUserId(myUser.getUserid());
+
+		try
+		{
+			LoginRecord loginRecord = new LoginRecord();
+			loginRecord.setId(UUID.randomUUID().toString().replace("-", "").toLowerCase());
+			loginRecord.setUserid(myUser.getUserid());
+			loginRecord.setUsername(myUser.getName());
+			loginRecord.setLoginip(request.getRemoteAddr());
+			loginRecord.setLogintime(new   Date ());
+			loginRecordMapper.insert(loginRecord);
+
+			TokenUtil tokenUtil = new  TokenUtil();
+			//生成Token
+			if (null == token) {
+
+				//第一次登陆
+				token =tokenUtil.creatToken(username) ;
+				tokenmapper.insertToken(token);
+
+			}else{
+				//登陆就更新Token信息
+
+
+				token = tokenUtil.creatToken(username) ;
+
+				tokenmapper.updateToken(token);
+			}
+		}
+		catch (Exception ex)
+		{
+			response.setMessage("登录失败" + ex.getMessage());
+			return  response;
+		}
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();//取时间
+		Calendar calendar = new GregorianCalendar();
+		calendar.setTime(date);
+		if (calendar.get(Calendar.HOUR_OF_DAY) < 6) {
+			calendar.add(Calendar.DATE, -1);
+			date = calendar.getTime();   //这个时间就是日期往后推一天的结果
+		}
+
+		StaffAttendanceDetail staffAttendanceDetail = staffAttendanceDetailMapper.selectCurrentUsableRecord(username,dateFormat.format(date));
+
+		if(staffAttendanceDetail != null)
+		{
+			response.setData("1");  //表示已经扫过工作位置
+			response.setMessage(myUser.getName() + " ### " + myUser.getRoleid() + " ### " + staffAttendanceDetail.getPlantid()
+					+ " ### " +  staffAttendanceDetail.getProcessid() + " ### " + staffAttendanceDetail.getLineid() + "###" + staffAttendanceDetail.getWorklocationid()
+					+ "###" + staffAttendanceDetail.getExtd1());
+		}
+		else
+		{
+			response.setData("2");//表示未扫过工作位置
+			response.setMessage(myUser.getName() + " ### " + myUser.getRoleid() + " ### " + myUser.getIndustrialplant_id()
+					+ " ### " +  myUser.getProductionprocess_id() + " ### " + myUser.getProductionline_id() + "###" + myUser.getWorklocation_id() + "###-1" );
 		}
 		response.setStatus(StatusEnum.ResponseStatus.Success.getIndex());
 		response.setToken(JSONObject.toJSON(token).toString());
